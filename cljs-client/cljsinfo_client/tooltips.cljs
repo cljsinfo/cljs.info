4fbd9261eed6e7c08eaf38614af860d4073817e4
Chris Oakman <chris@oakmac.com>
2014-10-29 01:04:55 -0500

    more work on tooltips

--- cljs-client/cljsinfo_client/tooltips.cljs
+++ cljs-client/cljsinfo_client/tooltips.cljs
@@ -1,44 +1,75 @@
 (ns cljsinfo-client.tooltips
   (:require
+    [clojure.set :refer [difference]]
     [cljsinfo-client.dom :refer [by-id set-html! show-el! hide-el!]]
     [cljsinfo-client.util :refer [js-log log uuid]]))
 
 (def $ js/jQuery)
 
 ;;------------------------------------------------------------------------------
+;; Helper
+;;------------------------------------------------------------------------------
+
+(defn- evt->tt-num
+  "Returns a tooltip-id from a JS event or nil if the tooltip was not found."
+  [js-evt]
+  (let [current-target (aget js-evt "currentTarget")
+        tooltip-num (.attr ($ current-target) "data-tooltip-id")
+        tooltip-id (str "tooltip-" tooltip-num)
+        tooltip-el (by-id tooltip-id)]
+    (if tooltip-el
+      tooltip-id)))
+
+;;------------------------------------------------------------------------------
 ;; Tooltips State
 ;;------------------------------------------------------------------------------
 
-(def active-tooltips (atom {}))
+(def hovered (atom #{}))
+(def pinned (atom #{}))
 
-(defn- on-change-tooltips [_ _ old-t new-t]
+(defn- on-change-hovered [_ _ old-ids new-ids]
+  (let [removed-ids (difference old-ids new-ids)
+        added-ids (difference new-ids old-ids)]
+    (doall (map hide-el! removed-ids))
+    (doall (map show-el! added-ids))))
+
+(defn- on-change-pinned [_ _ old-ids new-ids]
   ;; TODO: write me
   )
 
-(add-watch active-tooltips :change on-change-tooltips)
+(add-watch hovered :change on-change-hovered)
+(add-watch pinned :change on-change-pinned)
 
 ;;------------------------------------------------------------------------------
 ;; Init and Events
 ;;------------------------------------------------------------------------------
 
 (defn- on-mouseenter [js-evt]
-  (let [current-target (aget js-evt "currentTarget")
-        tooltip-num (.attr ($ current-target) "data-tooltip-id")
-        tooltip-id (str "tooltip-" tooltip-num)
-        tooltip-el (by-id tooltip-id)]
-    (when tooltip-el
-      (show-el! tooltip-id))))
+  (let [tooltip-id (evt->tt-num js-evt)]
+    (when (and tooltip-id
+               (not (contains? @pinned tooltip-id)))
+      (swap! hovered conj tooltip-id))))
 
 (defn- on-mouseleave [js-evt]
-  (let [current-target (aget js-evt "currentTarget")
-        tooltip-num (.attr ($ current-target) "data-tooltip-id")
-        tooltip-id (str "tooltip-" tooltip-num)
-        tooltip-el (by-id tooltip-id)]
-    (when tooltip-el
-      (hide-el! tooltip-id))))
+  (let [tooltip-id (evt->tt-num js-evt)]
+    (when (and tooltip-id
+               (not (contains? @pinned tooltip-id)))
+      (swap! hovered disj tooltip-id))))
+
+(defn- on-click [js-evt]
+  (let [tooltip-id (evt->tt-num js-evt)]
+    (if (contains? @pinned tooltip-id)
+      (swap! pinned disj tooltip-id)
+      (swap! pinned conj tooltip-id))))
+
+;;------------------------------------------------------------------------------
+;; Init
+;;------------------------------------------------------------------------------
 
 (defn init!
-  "Initialize tooltips."
+  "Initialize tooltip events."
   []
-  (.on ($ "body") "mouseenter" ".tooltip-0e91b" on-mouseenter)
-  (.on ($ "body") "mouseleave" ".tooltip-0e91b" on-mouseleave))
+  (doto ($ "body")
+    (.on "mouseenter" ".tooltip-0e91b" on-mouseenter)
+    (.on "mouseleave" ".tooltip-0e91b" on-mouseleave)
+    (.on "click" ".tooltip-0e91b" on-click)))
