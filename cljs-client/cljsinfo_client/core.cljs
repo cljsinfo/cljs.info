6986d833727afbc839bb3ed1360d1bbb7abc078d
Chris Oakman <chris@oakmac.com>
2014-12-09 18:49:11 -0600

    remove old cheatsheet stuff from cljs-client

--- cljs-client/cljsinfo_client/core.cljs
+++ cljs-client/cljsinfo_client/core.cljs
@@ -1,8 +1,6 @@
 (ns cljsinfo-client.core
   (:require
-    [cljsinfo-client.cheatsheet :as cheatsheet]
     [cljsinfo-client.threading-macro :as threading-macro]
-    [cljsinfo-client.tooltips :as tooltips]
     [cljsinfo-client.util :refer [js-log log]]))
 
 (def $ js/jQuery)
@@ -12,13 +10,9 @@
 ;;------------------------------------------------------------------------------
 
 (defn- init! [page]
-  ;; initialize tooltips on all pages
-  (tooltips/init!)
-
   ;; initialize a specific page
   (case page
     ;;"homepage" ()
-    "cheatsheet" (cheatsheet/init!)
     "threading-macro" (threading-macro/init!)
     nil))
 
