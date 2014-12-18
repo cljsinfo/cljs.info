e696c19184bed24bb3b3d9b3c2d318c3c608f979
Chris Oakman <chris@oakmac.com>
2014-12-17 21:45:19 -0600

    homepage design

--- cljs-server/cljsinfo_server/html.cljs
+++ cljs-server/cljsinfo_server/html.cljs
@@ -51,15 +51,35 @@
 ;; Homepage
 ;;------------------------------------------------------------------------------
 
-(hiccups/defhtml homepage []
-  (site-head "Home")
+(hiccups/defhtml top-nav-bar []
+  [:div.header-outer-a295e
+    [:div.header-inner-e9e98
+      [:div.left-1764b
+        [:a.main-link-bb01a {:href ""} "cljs" [:span.info-9c06a ".info"]]]
+      [:div.right-e461e
+        [:a.nav-link-890a3 {:href "/getting-started"} "Getting Started"]
+        [:a.nav-link-890a3 {:href "/docs"} "Documentation"]
+        [:a.nav-link-890a3 {:href "/tutorials"} "Tutorials"]
+        [:a.nav-link-890a3 {:href "/community"} "Community"]]
+      [:div.clr-43e49]]])
+
+(hiccups/defhtml jumbotron []
   [:div.title-outer-16d0f
     [:div.title-inner-df992
       [:h1.title-2febf "ClojureScript"]
-      [:h2.sub-d57b3 "JavaScript made simple"]]]
-  [:div.header-outer-a295e
-    [:div.header-inner-e9e98
-      [:a.main-link-bb01a {:href ""} "cljs" [:span.info-9c06a ".info"]]]]
+      [:h2.sub-d57b3 "JavaScript made simple"]
+      [:p.blurb-7fa5b
+        "ClojureScript is a functional programming language that targets "
+        "JavaScript. It comes with a rich set of data types, an extensive core "
+        "library, and a novel approach to state."]
+      [:p.blurb-7fa5b
+        "Programming for the browser" [:i.fa.fa-asterisk] " will never be the "
+        "same."]]])
+
+(hiccups/defhtml homepage []
+  (site-head "Home")
+  (jumbotron)
+  (top-nav-bar)
   (site-footer "homepage"))
 
 ;;------------------------------------------------------------------------------
