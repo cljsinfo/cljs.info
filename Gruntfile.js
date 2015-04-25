9dbd702188c6d7a132a5a68441bcce4d8088ef7a
Chris Oakman <chris.oakman@gmail.com>
2015-04-25 13:10:20 -0500

    remove animations parsing from Gruntfile (moved to separate repo)

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,55 +164,6 @@ function squeezeClasses() {
 }
 
 //------------------------------------------------------------------------------
-// Animation Files
-//------------------------------------------------------------------------------
-
-function parseFrame(txt) {
-  var lines = txt.split('\n'),
-    frameNumber = lines.shift().trim();
-
-  if (frameNumber === '') return;
-
-  console.log("===== " + frameNumber);
-
-  for (var y = 0; y < lines.length; y++) {
-    var line = lines[y].split('');
-    for (var x = 0; x < line.length; x++) {
-      var ch = line[x];
-
-      if (ch !== ' ') {
-        console.log(ch +  ' [' + (x + 1) + ' ' + (y + 1) + ']');
-      }
-    }
-  }
-}
-
-// this is a simple tool to help build the macro animations from text files
-function analyzeAnimation() {
-  // do nothing if an argument was not passed
-  if (this.args.length === 0) {
-    grunt.fail.warn('Please enter an argument to animation. ie: "grunt animation:foo"');
-    return;
-  }
-
-  // filename
-  var filename = 'animations/' + this.args[0] + '.txt';
-  if (! grunt.file.exists(filename)) {
-    grunt.fail.warn('Could not find ' + filename + '. Aborting...');
-    return;
-  }
-
-  var fileContents = grunt.file.read(filename),
-    frames = fileContents.split('=====');
-
-  for (var i = 0; i < frames.length; i++) {
-    parseFrame(frames[i]);
-  }
-
-  return;
-}
-
-//------------------------------------------------------------------------------
 // Doc Files
 //------------------------------------------------------------------------------
 
@@ -549,7 +500,6 @@ grunt.loadNpmTasks('grunt-contrib-copy');
 grunt.loadNpmTasks('grunt-contrib-less');
 grunt.loadNpmTasks('grunt-contrib-watch');
 
-grunt.registerTask('animation', analyzeAnimation);
 grunt.registerTask('build-docs', buildDocs);
 grunt.registerTask('build-gen-docs', buildGenDocs);
 grunt.registerTask('build-cheatsheet-sanity-check', buildCheatsheetSanityCheck);
